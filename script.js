83a5aec4aa5018b08f38747a83ef337ad78b90e9
Marco Rampin <rampin.marco.17@gmail.com>
2024-11-09 15:05:01 +0100

    functionalization of the script and fixed multiple addEventListener presence on container when a new grid is made

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 /*
-Add a button on the top of the screen that will send the user a popup asking for the number of
-squares (max 100) per side for the new grid. Once entered, the existing grid should be removed, and a new grid should be generated in the same total space as before (e.g., 960px wide) so that you’ve got a new sketch pad.
 
 EXTRA CREDIT
 randomize square color with each interaction
 implement a darkening effect each time the mouse hovers each cell*/
 
-//Create a page with 16x16 grid by using square divs (within a container) and flexbox.
 
-const initGrid = 16;
 
 function createFlex(numElements, elClass, parentEl, flexDirection){
     for (let i = 0; i < numElements; i++) {
@@ -20,12 +16,14 @@ function createFlex(numElements, elClass, parentEl, flexDirection){
     return document.querySelectorAll(`.${elClass}`);
 }
 
-function createGrid(numCells) {
-    const container = document.querySelector('#container');
+function createGrid(container, numCells) {
     const rows = Array.from(createFlex(numCells, 'row', container, 'vertical'));
     const cells = Array.from(rows, row => createFlex(numCells, 'cell', row, 'horizontal'));
 
+}
+
 //Set a hover effect so that the cells change color when the mouse hovers on them
+function addHoverEff(container){
     container.addEventListener('mouseover', (e) => {
 	if (e.target.classList[0] === 'cell') {
 	    e.target.classList.toggle('hoverEff');
@@ -33,4 +31,35 @@ function createGrid(numCells) {
     });
 }
 
-createGrid(initGrid);
+//Add a button that will ask new grid cell number per side (max 100) through a prompt. It should remove the previous grid and generate a new one
+
+function removeGrid() {
+    const rows = Array.from(document.querySelectorAll('.row'));
+    rows.forEach(row => row.remove());
+
+}
+
+function validateGridNum() {
+}
+
+function btnGridCreation(button) {
+    gridNum = prompt("How many cells per side? (max 100)");
+    removeGrid();
+    createGrid(container, gridNum);
+}
+
+function btnEvent(){
+    const gridBtn = document.querySelector('button');
+    gridBtn.addEventListener('click', () => btnGridCreation(this));
+}
+
+//Create a page with 16x16 grid by using square divs (within a container) and flexbox.
+function pageInit() {
+    const container = document.querySelector('#container');
+    let gridNum = 16;
+    createGrid(container, gridNum);
+    addHoverEff(container);
+    btnEvent();
+}
+
+pageInit();
