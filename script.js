8e68ee63ea60ee46074ccc44766f84fd6f04b302
Marco Rampin <rampin.marco.17@gmail.com>
2024-11-09 20:57:53 +0100

    Added darkening effect every time the mouse hovers on a cell

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-/*
-
-EXTRA CREDIT
-randomize square color with each interaction
-implement a darkening effect each time the mouse hovers each cell*/
-
-
-
 function createFlex(numElements, elClass, parentEl, flexDirection){
     for (let i = 0; i < numElements; i++) {
 	const div = document.createElement('div');
@@ -25,10 +17,37 @@ function createGrid(container, numCells) {
 //Set a hover effect so that the cells change color when the mouse hovers on them
 function addHoverEff(container){
     container.addEventListener('mouseover', (e) => {
-	if (e.target.classList[0] === 'cell') {
-	    e.target.classList.toggle('hoverEff');
+	if (e.target.classList.contains('cell')) {
+	    changeOpacity(e.target);
 	}
     });
+}
+
+//EXTRA: randomize square color with each interaction
+function getRandomValue() {
+    return Math.floor(Math.random() * 256);
+}
+
+function getRandomColor() {
+    const color = [];
+    for (let i = 0; i < 3; i++) {
+	color.push(getRandomValue());
+    }
+    return `rgb(${color[0]} ${color[1]} ${color[2]})`;
+}
+
+//EXTRA: implement a darkening effect each time the mouse hovers each cell*/
+function changeOpacity(element) {
+    let opacity = parseFloat(element.style.opacity);
+    if (opacity && opacity < 1.0) {
+	opacity +=  0.1;
+    } else if (opacity >= 1.0) {
+	opacity = 0;
+    } else {
+	opacity = 0.1;
+	element.style.cssText = `background-color: ${getRandomColor()};`;
+    }
+    element.style.opacity = opacity;
 }
 
 //Add a button that will ask new grid cell number per side (max 100) through a prompt. It should remove the previous grid and generate a new one
@@ -36,7 +55,6 @@ function addHoverEff(container){
 function removeGrid() {
     const rows = Array.from(document.querySelectorAll('.row'));
     rows.forEach(row => row.remove());
-
 }
 
 function isValidNum(gridNum) {
